7748aff39a9504192f0a7601c57e8629d685bc97
Muhammad Wisal <muhammadwisalkhanmv@gmail.com>
2024-06-07 02:26:25 +0500

    workflows

--- setup.py
+++ setup.py
@@ -1,11 +1,16 @@
 from setuptools import setup, find_packages
+import os
+
+# Read the contents of the README file
+with open(os.path.join(os.path.dirname(__file__), 'readme.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     author='Muhammad Wisal',
     author_email='wisal@lowerated.com',
-    description='Lowerated is an opensource library that allows you to Rate any item in a statistically accurate way',
-    long_description="If you plan on rating something based on some textual reviews avaialable or any information that criticises the thing"
-                     "Lowerated's rating algorithm will allow you to get a statistically accurate and true rating of that product or item.",
+    description='Lowerated is an opensource library that allows you to rate any item in a statistically accurate way',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     url='http://lowerated.com/products/',
     name='lowerated',
     version='0.1.0',
